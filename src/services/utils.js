a86c90e7cbdb400fa0400f12a1ead0b125757deb
Mariusz Materna <mariusz.materna@gmail.com>
2020-02-29 15:44:05 +0100

    poprawiony walidator daty

--- src/services/utils.js
+++ src/services/utils.js
@@ -4,7 +4,8 @@ export default {
   log,
   funcExists,
   stringPropertyExists,
-  replaceAll
+  replaceAll,
+  isDateValid
 }
 
 function rand (min, max) {
@@ -38,4 +39,25 @@ function replaceAll (text, key, replacement) {
   const regexp = '\\$\\{' + key + '\\}'
   /* eslint no-template-curly-in-string: "error" */
   return text.replace(new RegExp(regexp, 'g'), replacement)
+}
+
+function isDateValid (dateString) {
+  // Parse the date parts to integers
+  var parts = dateString.split('-')
+  var year = parseInt(parts[0], 10)
+  var month = parseInt(parts[1], 10)
+  var day = parseInt(parts[2], 10)
+
+  // Check the ranges of month and year
+  if (year < 1000 || year > 3000 || month === 0 || month > 12) {
+    return false
+  }
+  var monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  // Adjust for leap years
+  if (year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0)) {
+    monthLength[1] = 29
+  }
+
+  // Check the range of the day
+  return day > 0 && day <= monthLength[month - 1]
 }
